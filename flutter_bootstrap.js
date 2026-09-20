3a65c485628d205897507cfb2ce932197fcb2381
Lore Team <team@lore.app>
2026-09-20 08:08:46 +0300

    Bust browser and serviceWorker caches, safe dotenv handling

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -33,10 +33,8 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc030","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc030","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js?v=20260920_3"},{}]};
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "744604130" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
-  }
+  serviceWorkerSettings: null
 });
